458e5c84cee235be148154b654036e2ee28b86d0
AMWOLLO VICTOR <amwollo@yahoo.com>
2021-02-13 16:52:36 +0300

    add panel and title

--- index.php
+++ index.php
@@ -12,165 +12,173 @@
 
 
 <body>
-
     <div class="container">
-        <div class="row">
-            <div class="form-group col-sm-12" style="display: inline-block;">
-                <div class="radio">
-                    <label><input type="radio" name="measure_units" value="metres" checked="checked" />
-                        Metres</label>
+        <div class="col-sm-6 col-sm-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <h3 class="panel-title">Calculate Tiles Quantity</h3>
                 </div>
-                <div class="radio">
-                    <label>
-                        <input type="radio" name="measure_units" value="feet_inches" />
-                        Feets Inches</label>
+                <div class="panel-body">
+                    <div class="row">
+                        <div class="form-group col-sm-12" style="display: inline-block;">
+                            <div class="radio">
+                                <label><input type="radio" name="measure_units" value="metres" checked="checked" />
+                                    Metres</label>
+                            </div>
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="measure_units" value="feet_inches" />
+                                    Feets Inches</label>
+                            </div>
+                            <div class="radio">
+                                <label>
+                                    <input type="radio" name="measure_units" value="inches" />
+                                    Inches</label>
+                            </div>
+                        </div>
+                        <div class="form-group col-sm-12">
+                            <p>Total: <strong id="totalArea"></strong></p>
+                            <p>Total Cost : <strong id="totalCost"></strong></p>
+                            <p>Total Tiles : <strong id="totalTiles"></strong></p>
+                        </div>
+                    </div>
+                    <div class="row clearfix" id="tileCalc">
+                        <div class="measure col-md-12 table-responsive" id="measure-metres">
+                            <table class="table table-bordered table-hover table-sortable" id="tab_logic">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">
+                                        </th>
+                                        <th class="text-center">
+                                            Width
+                                        </th>
+                                        <th class="text-center">
+                                            Length
+                                        </th>
+                                        <th class="text-center">
+                                            Area
+                                        </th>
+                                        <th class="text-center">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr id='addr0' data-id="0" class="hidden">
+                                        <td data-name="label">
+                                            <label name='label0'></label>
+                                        </td>
+                                        <td data-name="width">
+                                            <input type="number" min="0" name='width0' placeholder='Width' class="width form-control" />
+                                        </td>
+                                        <td data-name="length">
+                                            <input type="number" min="0" name='length0' placeholder='Length' class="length form-control" />
+                                        </td>
+
+                                        <td data-name="area">
+                                            <input type="number" min="0" disabled="true" name='area0' class="area form-control" />
+                                        </td>
+
+                                        <td data-name="del">
+                                            <button name="del0" class='btn btn-danger glyphicon glyphicon-remove row-remove'></button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <a id="add_row" class="btn btn-primary float-right">Add Row</a>
+                        </div>
+
+                        <div class="measure col-md-12 table-responsive" id="measure-feet_inches">
+                            <table class="table table-bordered table-hover table-sortable" id="tab_feet_inches">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">
+                                        </th>
+                                        <th class="text-center" colspan="2">
+                                            Width
+                                        </th>
+                                        <th class="text-center" colspan="2">
+                                            Length
+                                        </th>
+                                        <th class="text-center">
+                                            Area
+                                        </th>
+                                        <th class="text-center">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr id='addr0' data-id="0" class="hidden">
+                                        <td data-name="label">
+                                            Wall
+                                        </td>
+                                        <td data-name="feetwid">
+                                            <input type="number" min="0" name='width0' placeholder='Feet' class="width form-control" />
+                                        </td>
+                                        <td data-name="inchwidth">
+                                            <input type="number" min="0" name='winch0' placeholder='Inch' class="width form-control" />
+                                        </td>
+                                        <td data-name="feetleng">
+                                            <input type="number" min="0" name='length0' placeholder='Feet' class="length form-control" />
+                                        </td>
+                                        <td data-name="inchlength">
+                                            <input type="number" min="0" name='linch0' placeholder='Inch' class="length form-control" />
+                                        </td>
+
+                                        <td data-name="area">
+                                            <input type="number" min="0" disabled="true" name='area0' class="area form-control" />
+                                        </td>
+
+                                        <td data-name="del">
+                                            <button name="del0" class='btn btn-danger glyphicon glyphicon-remove row-remove'></button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <a id="add_feet_inch" class="btn btn-primary float-right">Add Row</a>
+                        </div>
+
+                        <div class="measure col-md-12 table-responsive" id="measure-inches">
+                            <table class="table table-bordered table-hover" id="tab_inches">
+                                <thead>
+                                    <tr>
+                                        <th class="text-center">
+                                        </th>
+                                        <th class="text-center">
+                                            Width
+                                        </th>
+                                        <th class="text-center">
+                                            Length
+                                        </th>
+                                        <th class="text-center">
+                                            Area
+                                        </th>
+                                        <th class="text-center">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr id='addr0' data-id="0" class="hidden">
+                                        <td data-name="label">
+                                            Wall
+                                        </td>
+                                        <td data-name="widinch">
+                                            <input type="number" min="0" name='winch0' placeholder='Inch' class="width form-control" />
+                                        </td>
+                                        <td data-name="leninch">
+                                            <input type="number" min="0" name='linch0' placeholder='Inch' class="length form-control" />
+                                        </td>
+
+                                        <td data-name="area">
+                                            <input type="number" min="0" disabled="true" name='area0' class="area form-control" />
+                                        </td>
+
+                                        <td data-name="del">
+                                            <button name="del0" class='btn btn-danger glyphicon glyphicon-remove row-remove'></button>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                            <a id="add_inch_row" class="btn btn-primary float-right">Add Row</a>
+                        </div>
+                    </div>
                 </div>
-                <div class="radio">
-                    <label>
-                        <input type="radio" name="measure_units" value="inches" />
-                        Inches</label>
-                </div>
-            </div>
-            <div class="form-group col-sm-12">
-                <p>Total: <strong id="totalArea"></strong></p>
-                <p>Total Cost : <strong id="totalCost"></strong></p>
-                <p>Total Tiles : <strong id="totalTiles"></strong></p>
-            </div>
-        </div>
-        <div class="row clearfix" id="tileCalc">
-            <div class="measure col-md-12 table-responsive" id="measure-metres">
-                <table class="table table-bordered table-hover table-sortable" id="tab_logic">
-                    <thead>
-                        <tr>
-                            <th class="text-center">
-                            </th>
-                            <th class="text-center">
-                                Width
-                            </th>
-                            <th class="text-center">
-                                Length
-                            </th>
-                            <th class="text-center">
-                                Area
-                            </th>
-                            <th class="text-center">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr id='addr0' data-id="0" class="hidden">
-                            <td data-name="label">
-                                <label name='label0'></label>
-                            </td>
-                            <td data-name="width">
-                                <input type="number" min="0" name='width0' placeholder='Width' class="width form-control" />
-                            </td>
-                            <td data-name="length">
-                                <input type="number" min="0" name='length0' placeholder='Length' class="length form-control" />
-                            </td>
-
-                            <td data-name="area">
-                                <input type="number" min="0" disabled="true" name='area0' class="area form-control" />
-                            </td>
-
-                            <td data-name="del">
-                                <button name="del0" class='btn btn-danger glyphicon glyphicon-remove row-remove'></button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <a id="add_row" class="btn btn-primary float-right">Add Row</a>
-            </div>
-
-            <div class="measure col-md-12 table-responsive" id="measure-feet_inches">
-                <table class="table table-bordered table-hover table-sortable" id="tab_feet_inches">
-                    <thead>
-                        <tr>
-                            <th class="text-center">
-                            </th>
-                            <th class="text-center" colspan="2">
-                                Width
-                            </th>
-                            <th class="text-center" colspan="2">
-                                Length
-                            </th>
-                            <th class="text-center">
-                                Area
-                            </th>
-                            <th class="text-center">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr id='addr0' data-id="0" class="hidden">
-                            <td data-name="label">
-                                Wall
-                            </td>
-                            <td data-name="feetwid">
-                                <input type="number" min="0" name='width0' placeholder='Feet' class="width form-control" />
-                            </td>
-                            <td data-name="inchwidth">
-                                <input type="number" min="0" name='winch0' placeholder='Inch' class="width form-control" />
-                            </td>
-                            <td data-name="feetleng">
-                                <input type="number" min="0" name='length0' placeholder='Feet' class="length form-control" />
-                            </td>
-                            <td data-name="inchlength">
-                                <input type="number" min="0" name='linch0' placeholder='Inch' class="length form-control" />
-                            </td>
-
-                            <td data-name="area">
-                                <input type="number" min="0" disabled="true" name='area0' class="area form-control" />
-                            </td>
-
-                            <td data-name="del">
-                                <button name="del0" class='btn btn-danger glyphicon glyphicon-remove row-remove'></button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <a id="add_feet_inch" class="btn btn-primary float-right">Add Row</a>
-            </div>
-
-            <div class="measure col-md-12 table-responsive" id="measure-inches">
-                <table class="table table-bordered table-hover" id="tab_inches">
-                    <thead>
-                        <tr>
-                            <th class="text-center">
-                            </th>
-                            <th class="text-center">
-                                Width
-                            </th>
-                            <th class="text-center">
-                                Length
-                            </th>
-                            <th class="text-center">
-                                Area
-                            </th>
-                            <th class="text-center">Action</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        <tr id='addr0' data-id="0" class="hidden">
-                            <td data-name="label">
-                                Wall
-                            </td>
-                            <td data-name="widinch">
-                                <input type="number" min="0" name='winch0' placeholder='Inch' class="width form-control" />
-                            </td>
-                            <td data-name="leninch">
-                                <input type="number" min="0" name='linch0' placeholder='Inch' class="length form-control" />
-                            </td>
-
-                            <td data-name="area">
-                                <input type="number" min="0" disabled="true" name='area0' class="area form-control" />
-                            </td>
-
-                            <td data-name="del">
-                                <button name="del0" class='btn btn-danger glyphicon glyphicon-remove row-remove'></button>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <a id="add_inch_row" class="btn btn-primary float-right">Add Row</a>
             </div>
         </div>
     </div>
